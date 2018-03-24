b0303be61252d94f4b83c4db54062bfbd549eba7
pjmike <45628852pj>
2018-03-24 20:46:44 +0800

    add cors config

--- AA-time-web/src/main/java/com/ctg/aatime/commons/config/MyWebMvcConfiguration.java
+++ AA-time-web/src/main/java/com/ctg/aatime/commons/config/MyWebMvcConfiguration.java
@@ -2,7 +2,9 @@ package com.ctg.aatime.commons.config;
 
 import com.ctg.aatime.web.interceptor.Interceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,8 +16,36 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MyWebMvcConfiguration implements WebMvcConfigurer {
     @Autowired
     private Interceptor interceptor;
+
+    /**
+     * 重写父类的拦截器方法
+     *
+     * @param registry
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        //添加拦截路径
         registry.addInterceptor(interceptor).addPathPatterns("/index");
+    }
+
+    /**
+     * 重写父类的跨域请求接口
+     *
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //添加映射路径
+        registry.addMapping("/**")
+                //放行哪些原始域
+                .allowedOrigins("*")
+                //放行哪些请求头
+                .allowedHeaders("*")
+                //是否发送cookie信息
+                .allowCredentials(true)
+                //放行哪些请求方法
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                //暴露哪些请求头
+                .exposedHeaders("Header1", "Header2");
     }
 }
--- AA-time-web/src/main/java/com/ctg/aatime/commons/config/WebSocketConfigRelay.java
+++ AA-time-web/src/main/java/com/ctg/aatime/commons/config/WebSocketConfigRelay.java
@@ -1,6 +1,9 @@
 package com.ctg.aatime.commons.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
@@ -8,8 +11,15 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
  * @author pjmike
  * @create 2018-03-24 17:56
  */
+@Configuration
+@EnableWebSocketMessageBroker
 public class WebSocketConfigRelay implements WebSocketMessageBrokerConfigurer {
-
+    @Value("${spring.rabbitmq.host")
+    private String host;
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+    @Value("${spring.rabbitmq.password}")
+    private String password;
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
@@ -23,9 +33,9 @@ public class WebSocketConfigRelay implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableStompBrokerRelay("/topic")
-                .setRelayHost("39.106.63.214")
+                .setRelayHost(host)
                 .setRelayPort(5672)
-                .setClientLogin("guest")
-                .setClientPasscode("guest");
+                .setClientLogin(username)
+                .setClientPasscode(password);
     }
 }
